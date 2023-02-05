bbc04b969d284b5cad49d3b0bf1a638ebc637f6f
James Rampton <me@jamesrampton.com>
2023-02-05 21:20:23 +0000

    Duplicate functional list as a component

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
 use leptos::*;
 
 fn main() {
-    let cat_names = vec!["Beans", "Basil", "Oliver"];
+    let cat_names_func = vec!["Beans", "Basil", "Oliver", "Funcy"];
+    let cat_names = vec!["Beans", "Basil", "Oliver", "Compy"];
 
     mount_to_body(|cx| {
         view! { cx,
-            <h1>"Great cat names"</h1>
+            <h1>"Great cat names (func)"</h1>
             <ul>
-                {list_names(cx,cat_names)}
+                {list_names_func(cx,cat_names_func)}
             </ul>
+            <ListNames cat_names />
         }
     })
 }
 
-fn list_names(cx: Scope, cat_names: Vec<&'static str>) -> Vec<View> {
+fn list_names_func(cx: Scope, cat_names: Vec<&'static str>) -> Vec<View> {
     cat_names
         .into_iter()
         .map(move |name| view! {cx, <li>{name}</li>})
         .map(move |li| li.into_view(cx))
         .collect()
+}
+
+#[component]
+fn ListNames(cx: Scope, cat_names: Vec<&'static str>) -> impl IntoView {
+    let list_items: Vec<_> = cat_names
+        .into_iter()
+        .map(move |name| view! {cx, <li>{name}</li>})
+        .collect();
+
+    view! {cx,
+        <h1>"Great cat names (component)"</h1>
+        <ul>
+            {list_items}
+        </ul>
+    }
 }
 
 // #[component]
