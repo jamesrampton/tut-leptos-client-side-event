fc6c64aca2d9fe08243d96877bec90e4bd45751f
James Rampton <me@jamesrampton.com>
2023-02-05 17:48:09 +0000

    Add our first component

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ fn main() {
         view! {
             cx,
             <h1>"Hello, world!"</h1>
+            <NiceAffirmation />
         }
     });
+}
+
+#[component]
+pub fn NiceAffirmation(cx: Scope) -> impl IntoView {
+    view! {
+        cx,
+        <p>"You look nice today!"</p>
+    }
 }
